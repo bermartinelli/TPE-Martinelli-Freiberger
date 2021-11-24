e29f0e00180c0948619a43cd466c173b2942fe09
Miqueas Freiberger <miqueasfreiberger@gmail.com>
2021-11-24 17:25:18 -0300

    actualizacion DDBB

--- db_mysql/db_libros.sql
+++ db_mysql/db_libros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2021 a las 22:52:31
+-- Tiempo de generación: 24-11-2021 a las 21:24:27
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
@@ -32,26 +32,58 @@ CREATE TABLE `autor` (
   `nombre` varchar(60) NOT NULL,
   `nacimiento` date NOT NULL,
   `muerte` date NOT NULL,
-  `nacionalidad` varchar(40) NOT NULL
+  `nacionalidad` varchar(40) NOT NULL,
+  `imagen` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `autor`
 --
 
-INSERT INTO `autor` (`id_autor`, `nombre`, `nacimiento`, `muerte`, `nacionalidad`) VALUES
-(1, 'Stephen King', '1947-09-21', '0000-00-00', 'Estadounidense'),
-(2, 'J K Rowling', '1965-07-31', '0000-00-00', 'Britanica'),
-(3, 'Gabriel García Marquez', '1927-03-06', '2014-04-17', 'Colombiano'),
-(4, 'Ernesto Sábato', '1911-06-24', '2011-04-30', 'Argentino'),
-(5, 'Florencia Bonelli', '1971-05-05', '0000-00-00', 'Argentina'),
-(6, 'George R R Martin', '1948-09-20', '0000-00-00', 'Estadounidense'),
-(7, 'Edgar Allan Poe', '1809-01-19', '1849-10-07', 'Estadounidense'),
-(8, 'J R R Tolkien', '1892-01-03', '1973-09-02', 'Britanico'),
-(9, 'Stieg Larsson', '1954-08-15', '2004-11-09', 'Sueco'),
-(10, 'Paulo Coelho', '1947-08-24', '0000-00-00', 'Brasileño'),
-(11, 'Dan Brown', '1964-06-22', '0000-00-00', 'Estadounidense'),
-(13, 'Patrick Suskind', '1949-03-26', '0000-00-00', 'Aleman');
+INSERT INTO `autor` (`id_autor`, `nombre`, `nacimiento`, `muerte`, `nacionalidad`, `imagen`) VALUES
+(1, 'Stephen Edwing King', '1947-09-21', '0000-00-00', 'Estadounidense', 'img/author/ 619d85e1e5854.jpg'),
+(2, 'J K Rowling', '1965-07-31', '0000-00-00', 'Britanica', ''),
+(3, 'Gabriel García Marquez', '1927-03-06', '2014-04-17', 'Colombiano', ''),
+(4, 'Ernesto Sábato', '1911-06-24', '2011-04-30', 'Argentino', ''),
+(5, 'Florencia Bonelli', '1971-05-05', '0000-00-00', 'Argentina', ''),
+(6, 'George R R Martin', '1948-09-20', '0000-00-00', 'Estadounidense', ''),
+(7, 'Edgar Allan Poe', '1809-01-19', '1849-10-07', 'Estadounidense', ''),
+(8, 'J R R Tolkien', '1892-01-03', '1973-09-02', 'Britanico', ''),
+(9, 'Stieg Larsson', '1954-08-15', '2004-11-09', 'Sueco', ''),
+(10, 'Paulo Coelho', '1947-08-24', '0000-00-00', 'Brasileño', ''),
+(11, 'Dan Brown', '1964-06-22', '0000-00-00', 'Estadounidense', ''),
+(13, 'Patrick Suskind', '1949-03-26', '0000-00-00', 'Aleman', ''),
+(14, 'Oscar Wilde', '1854-10-16', '1900-11-30', 'Irlandes', 'img/author/ 619d848dd5f4bjpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `puntuacion` tinyint(5) NOT NULL,
+  `usuario` int(11) NOT NULL,
+  `id_libro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `comentario`, `puntuacion`, `usuario`, `id_libro`) VALUES
+(1, 'Muy bueno el libro', 4, 2, 20),
+(2, 'Me resulto muy interesante pero es extenso', 3, 2, 33),
+(5, 'No me gusto el libro, me resulto muy pesado y largo', 1, 2, 3),
+(6, 'Interesante libro', 1, 2, 3),
+(7, 'Muy buen libro, interesante y facil de leer', 4, 2, 5),
+(8, 'Muy buen libro, interesante y tiene buenos personajes', 5, 2, 6),
+(9, 'Me resulto aburrido y pesado de leer.', 1, 2, 19),
+(10, 'muy buen libro.', 5, 6, 3),
+(11, 'asdads', 2, 2, 3),
+(12, 'asdads', 1, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -123,7 +155,8 @@ INSERT INTO `libros` (`id_libros`, `nombre`, `genero`, `capitulos`, `editorial`,
 (56, 'Tormenta de espadas', 'Literatura fantástica', 1184, 'PLAZA & JANES', 2000, 6),
 (57, 'Festín de cuervos', 'Literatura fantástica', 1133, 'PLAZA & JANES', 2005, 6),
 (58, 'Danza de dragones', 'Literatura fantástica', 872, 'PLAZA & JANES', 2011, 6),
-(60, 'El general en su laberinto', 'Novela Historica', 20, 'Alfred A. Knopf', 1989, 3);
+(60, 'El general en su laberinto', 'Novela Historica', 20, 'Alfred A. Knopf', 1989, 3),
+(61, 'El fantasma de canterville', 'Ficcion Gotica', 7, 'The Court and Society Review', 1887, 14);
 
 -- --------------------------------------------------------
 
@@ -145,7 +178,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `email`, `username`, `password`, `rol`) VALUES
 (1, 'admin@mail.com', 'admin', '$2a$12$CG6n2o8hEfD/vTa/KC.T.enw/VuOsXm.JngiG94xm5oYFi.1hH22C', 1),
-(2, 'mique@mail.com', 'mique5', '$2y$10$2v/2x3fPN1.sJV37onjD2eygqv.0Jor7b6m0a2zEAeV5VO8DAZldW', 0);
+(2, 'mique@mail.com', 'mique5', '$2y$10$2v/2x3fPN1.sJV37onjD2eygqv.0Jor7b6m0a2zEAeV5VO8DAZldW', 0),
+(6, 'prueba@mail.com', 'userPrueba', '$2y$10$7HSBqZSlP70twQH6kgM.v.y6UVNP3i9E63WfcElrrlfPSVuoX02Um', 0);
 
 --
 -- Índices para tablas volcadas
@@ -158,6 +192,14 @@ ALTER TABLE `autor`
   ADD PRIMARY KEY (`id_autor`);
 
 --
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `id_libro` (`id_libro`);
+
+--
 -- Indices de la tabla `libros`
 --
 ALTER TABLE `libros`
@@ -168,7 +210,8 @@ ALTER TABLE `libros`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -178,23 +221,36 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `autor`
 --
 ALTER TABLE `autor`
-  MODIFY `id_autor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_autor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id_libros` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id_libros` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libros`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`);
 
 --
 -- Filtros para la tabla `libros`
